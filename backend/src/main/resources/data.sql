e7a6000b533cd3ba9ae625a855724376c8a067b1
Eduardo Rodrigues <oraeduardo.gmail.com>
2022-06-14 13:51:14 -0300

    Notification, Corse, Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,10 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('My first notification', TIMESTAMP WITH TIME ZONE '2022-06-14T12:16:07.12345Z', true, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg',1L);
+
+INSERT INTO tb_corse(name, img_uri, img_gray_uri) VALUES ('BootCamp HTML', 'https://www.smartpassiveincome.com/wp-content/uploads/2020/04/How-to-Create-an-Online-Course.png', 'https://i.ytimg.com/vi/RDoXrMCzp-I/maxresdefault.jpg');
+
+INSERT INTO tb_offer(edition, start_moment, end_moment, corse_id) VALUES ('Turma 1.0', NOW(), TIMESTAMP WITH TIME ZONE '2022-12-31T12:00:00.12345Z', 1L);
+INSERT INTO tb_offer(edition, start_moment, end_moment, corse_id) VALUES ('Turma 2.0', NOW(), TIMESTAMP WITH TIME ZONE '2022-12-31T12:00:00.12345Z', 1L);
